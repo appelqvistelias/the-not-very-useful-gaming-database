1d1fce3bf2d710f0cb45f91bccf13b8ab8a67917
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-12-06 14:52:22 +0100

    made code mor modular

--- script/script.js
+++ script/script.js
@@ -15,7 +15,7 @@ const monsterLoot = document.querySelector('.monster-loot');
 const monsterDescription = document.querySelector('.monster-description');
 
 
-const getHruleMonster = async (userSearchValue) => {
+const fetchMonsterFromAPI = async (userSearchValue) => {
     const hruleURL =`https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${userSearchValue}`;
     
     try {
@@ -27,34 +27,47 @@ const getHruleMonster = async (userSearchValue) => {
 
     const monster = await response.json();
 
-    console.log(monster);
+    return monster;
     
+} catch (error) {
+    console.error('Something went wrong', error);
+    monsterName.textContent = 'Could not fetch monster data';
+    monsterLocations.textContent = 'Could not fetch monster locations';
+    monsterLoot.textContent = 'Could not fetch monster loot';
+    monsterDescription.textContent = 'Could not fetch monster description';
+}
+};
+
+const monsterToObj = (monster) => {
     const info = {
         monsterName: toTitleCase(monster.data.name) || 'Unknown monster name!',
         locations: (monster.data.common_locations || []).join(', ') || 'No locations found!',
         loot: (monster.data.drops || []).map(toTitleCase).join(', ') || 'No loot found!',
         description: monster.data.description || 'No description data available!'
     };
+    return info;
+}
 
-    monsterName.textContent = info.monsterName;
-    monsterLocations.textContent = info.locations;
-    monsterLoot.textContent = info.loot;
-    monsterDescription.textContent = info.description;
+const addMonsterToDom = (monster) => {
+    monsterName.textContent = monster.monsterName;
+    monsterLocations.textContent = monster.locations;
+    monsterLoot.textContent = monster.loot;
+    monsterDescription.textContent = monster.description;
+}
 
-    } catch (error) {
-        console.error('Something went wrong', error);
-        monsterName.textContent = 'Could not fetch monster data';
-        monsterLocations.textContent = 'Could not fetch monster locations';
-        monsterLoot.textContent = 'Could not fetch monster loot';
-        monsterDescription.textContent = 'Could not fetch monster description';
-    }
-};
-
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', async () => {
     const userSearchValue = searchField.value.trim();
     if (userSearchValue) {
         const sanitizedSearchValue = encodeURIComponent(userSearchValue.toLowerCase());
-        getHruleMonster(sanitizedSearchValue);
+        
+        // fetch from api
+        const data = await fetchMonsterFromAPI(sanitizedSearchValue);
+
+        // data -> obj
+        const monster = monsterToObj(data);
+        
+        // obj -> add dom
+        addMonsterToDom(monster);
     } else {
         alert('You need to name a monster!');
     }
